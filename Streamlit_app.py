faacb0faf17d6f588e8913bdc92127a60f29d4f5
Kanagadurgae08 <durgavarsha0826@gmail.com>
2025-02-27 12:35:09 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
